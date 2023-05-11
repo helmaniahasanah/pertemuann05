6ab443bfde0afd731ef2c1298edaa4124ae9356e
helmaniahasanah <129954900+helmaniahasanah@users.noreply.github.com>
2023-05-11 11:59:29 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/pertemuann05/main.java
@@ -0,0 +1,30 @@
+package com.mycompany.pertemuann05;
+
+public class main{
+    public static void main(String[] args){
+      
+    
+      pakaian pakaian =new pakaian();
+      pakaian.size = "M";
+      pakaian.warna = "putih";
+      pakaian.harga= 50000;
+              
+      pakaian pakaian1 =new pakaian();
+      pakaian1.size = "L";
+      pakaian1.warna = "Merah";
+      pakaian1.harga= 60000;
+              
+      pakaian pakaian2 =new pakaian();
+      pakaian2.size = "XL";
+      pakaian2.warna = "Hitam";
+      pakaian2.harga= 75000;
+              
+              
+              pakaian.info();
+              pakaian1.info();
+              pakaian2.info();
+              
+   
+    
+    }
+}
---
+++ src/main/java/com/mycompany/pertemuann05/pakaian.java
@@ -0,0 +1,13 @@
+package com.mycompany.pertemuann05;
+
+public class pakaian{
+   String size;
+   String warna;
+  int harga;
+   
+   void info(){
+       System.out.println("warna pakaian : " + warna+" "+ "size yg di pilih : "+size+" "+"Harga pakaian :"+harga );
+   }
+}
+
+
